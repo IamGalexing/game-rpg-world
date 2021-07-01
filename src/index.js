074f47d4d71dc97fbb543120e860de4f6c42df76
Oleksandr_Gneushev <galexing@gmail.com>
2021-07-01 15:18:30 +0200

    add watch dev script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './index.scss';
+
+const canvas = document.getElementById('game');
+
+const ctx = canvas.getContext('2d');
+
+ctx.strokeStyle = 'red';
+ctx.lineWidht = 5;
+ctx.strokeRect(20, 20, 200, 100);
